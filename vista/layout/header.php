5fcd9f88bc737ad2553eb96553912eb1fe1ce715
AdrianSamayoa9999 <idarkraiden01@hotmail.com>
2024-10-20 23:41:27 -0600

    header.php

--- vista/layout/header.php
+++ vista/layout/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proyecto Almacen</title>
+    <title>Proyecto Web</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="vista/css/style.css">
 </head>
@@ -38,22 +38,22 @@
         <nav>
             <ul class="nav justify-content-center">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Paquetes</a>
+                    <a class="nav-link active" aria-current="page" href="paquetes.php">Paquetes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Vuelos</a>
+                    <a class="nav-link" href="vuelos.php">Vuelos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Promociones</a>
+                    <a class="nav-link" href="promociones.php">Promociones</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Hoteles</a>
+                    <a class="nav-link active" aria-current="page" href="hoteles.php">Hoteles</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Alquiler de sitio</a>
+                    <a class="nav-link active" aria-current="page" href="alquilerdesitio.php">Alquiler de sitio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Ayuda</a>
+                    <a class="nav-link active" aria-current="page" href="ayuda.php">Ayuda</a>
                 </li>
             </ul>
         </nav>
